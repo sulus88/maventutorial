d7f03dd827124664296de8995326eb6dee24d295
Suna Ulus <suna.ulus@gmail.com>
2023-01-13 20:56:43 -0600

    created maven project but couldn't run it

---
+++ SpringHelloWorldApp/src/test/java/org/launchcode/SpringHelloWorldApp/SpringHelloWorldAppApplicationTests.java
@@ -0,0 +1,14 @@
+package org.launchcode.SpringHelloWorldApp;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
